9b5d7ca87c3aaf9cca647da196dc3dcb61d99089
IIIsa <SuharevIA@mer.ci.nsu.ru>
2024-06-03 23:20:20 +0300

    Commit auto clicker

---
+++ main.py
@@ -0,0 +1,17 @@
+import keyboard
+import mouse
+import time
+
+
+def change():
+    global work
+    work = not work
+
+
+work = False
+keyboard.add_hotkey('`', change)
+
+while True:
+    if work:
+        mouse.click(button='left')
+        time.sleep(0.00000000000000000000000000000000000000001)
